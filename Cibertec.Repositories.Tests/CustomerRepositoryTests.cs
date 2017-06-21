5daf9dfe11313540c36c6353e9066a7f12626059
Cristian Arevalo <cristian.arevalo81@gmail.com>
2017-06-20 22:29:27 -0500

    Data moqueada y FluentAssertions

--- Cibertec.Repositories.Tests/CustomerRepositoryTests.cs
+++ Cibertec.Repositories.Tests/CustomerRepositoryTests.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xunit;
+using FluentAssertions;
+using Cibertec.Models;
 
 namespace Cibertec.Repositories.Tests
 {
@@ -12,15 +14,37 @@ namespace Cibertec.Repositories.Tests
         private readonly IUnitOfWork _unit;
         public CustomerRepositoryTests()
         {
-            _unit = new CibertecUnitOfWork(ConfigSettings.ConnectionString);
+            //_unit = new CibertecUnitOfWork(ConfigSettings.ConnectionString);
+            _unit = MockedUnitOfWork.GetUnitOfWork();
         }
 
         [Fact(DisplayName = "[CustomerRepositoryTests] Get All Customer")]
         public void test()
         {
             var result = _unit.Customers.GetAll();
-            Assert.NotNull(result);
-            Assert.True(result.Count() > 0);
+            result.Should().NotBeNull(); //el resultado deberia no ser nulo
+            result.Count().Should().BeGreaterThan(0); //el resultado debe de ser mayor a cero            
+        }
+
+        [Fact(DisplayName = "[CustomerRepositoryTests] Get Entity By ID Customer")]
+        public void test1()
+        {
+            var result = _unit.Customers.GetEntityById(1);
+            result.Should().NotBeNull();            
+        }
+
+        [Fact(DisplayName = "[CustomerRepositoryTests] Insert Customer")]
+        public void Insert_Customer()
+        {
+            var result = _unit.Customers.Insert(null);
+            result.Should().BeGreaterThan(0);
+        }
+
+        [Fact(DisplayName = "[CustomerRepositoryTests] Fail Insert Customer")]
+        public void Insert_Customer_Wrong()
+        {
+            var result = _unit.Customers.Insert(new customer());
+            result.Should().Be(0);
         }
     }
 }
---
+++ Cibertec.Repositories.Tests/MockedUnitOfWork.cs
@@ -0,0 +1,67 @@
+﻿using Cibertec.Models;
+using Cibertec.UnitOfWork;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cibertec.Repositories.Tests
+{
+    public class MockedUnitOfWork
+    {
+        public static IUnitOfWork GetUnitOfWork()
+        {
+            Mock<IUnitOfWork> unit = new Mock<IUnitOfWork>();
+            unit.ConfigureCustomer();
+            return unit.Object;
+        }
+    }
+
+    //extendiendo MockedUnitOfWork(palabra clave this)
+    //todas las extensiones son del titpo estatico
+    public static class MockedUnitOfWorkExtensions
+    {
+        public static Mock<IUnitOfWork> ConfigureCustomer(this Mock<IUnitOfWork> mock)
+        {
+            mock.Setup(c => c.Customers.GetAll()).Returns(
+                new List<customer>
+                {
+                    new customer
+                    {
+                        Id = 1,
+                        City = "Lima",
+                        Country = "Peru",
+                        FirstName = "Cristian",
+                        LastName = "Arevalo Solis",
+                        Phone = "955782336"
+                    },
+                    new customer
+                    {
+                        Id = 2,
+                        City = "Cajamarca",
+                        Country = "Peru",
+                        FirstName = "Ana",
+                        LastName = "Machuca Arevalo",
+                        Phone = "955782336"
+                    }
+                });
+
+
+            mock.Setup(c => c.Customers.GetEntityById(1)).Returns(
+                new customer
+                {
+                    Id = 1,
+                    City = "Lima",
+                    Country = "Peru",
+                    FirstName = "Cristian",
+                    LastName = "Arevalo Solis",
+                    Phone = "955782336"
+                });
+
+            mock.Setup(c => c.Customers.Insert(null)).Returns(1);
+
+            return mock;
+        }
+    }
+
+}
