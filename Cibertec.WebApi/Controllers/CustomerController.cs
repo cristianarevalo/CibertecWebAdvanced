52e712697b011dd8d63dafe9634e00ce3f736dcf
Cristian Arevalo <cristian.arevalo81@gmail.com>
2017-06-27 22:31:49 -0500

    Implementacion Web Api

---
+++ Cibertec.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using Cibertec.Models;
+using Cibertec.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cibertec.WebApi.Controllers
+{
+    [Route("customer")]
+    public class CustomerController: Controller
+    {
+        private readonly IUnitOfWork _unit;
+        public CustomerController(IUnitOfWork unit)
+        {
+            _unit = unit;
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return Ok(_unit.Customers.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] customer customer)
+        {
+            return Ok(_unit.Customers.Insert(customer));
+        }
+
+    }
+}
