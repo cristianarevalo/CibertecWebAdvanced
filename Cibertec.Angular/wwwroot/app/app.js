ee43a0af299298cb61ea7c0c977662beef2de1f2
Cristian Arevalo <cristian.arevalo81@gmail.com>
2017-07-06 22:22:20 -0500

    Creacion de una aplicacion con angular

---
+++ Cibertec.Angular/wwwroot/app/app.js
@@ -0,0 +1,5 @@
+﻿(function () { //auto ejecucion de IIFE
+    'use strict'; //para que las variables sea declaradas
+
+    angular.module('app', ['ui.router']); //creamos el modulo app e injectando la libreria ui.router
+})(); //() ejecuta la funcion
---
+++ Cibertec.Angular/wwwroot/app/app.routes.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    'use strict'; //Exijimos que valide las variables
+
+    angular.module('app')
+        .config(routeConfig); //configurando el modulo, setea la funcion routeConfig
+
+    routeConfig.$inject = ['$stateProvider', '$urlRouterProvider']; //injección de dependiencias, esta entre comillas para el DOM no lo cambie cuando se minifique
+    //$stateProvider: permite saber el estado en el cual estamos trabajando, cualquier que no concuerda se va al 'otherwise'
+    //$urlRouterProvider:
+    //cuando se navega antepone los simbolos # y !: para busqueda SEO(Buscadores, ranking)
+
+    function routeConfig($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state("home", {
+                url: "/home",
+                templateUrl: 'app/home.html'
+            })
+            .state("login", {
+                url: "/login",
+                templateUrl: 'app/login.html'
+            })
+            .state("otherwise", {
+                url: '*path',
+                templateUrl: 'app/home.html'
+            });
+    }
+
+})();
